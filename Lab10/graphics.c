b65e6427308f1c5f062b03f181337fc0032ef9f6
Joshua <jdong42@gmail.com>
2013-12-03 05:09:54 -0600

    [graphics] Finished 3d cube shader, now needs debugging.

--- Lab10/graphics.c
+++ Lab10/graphics.c
@@ -6,12 +6,17 @@
 #define HEIGHT 240
 #define BLACK 0x0000
 #define WHITE 0xFFFF
+#define RED 0xF800
 
+#define clear_cubes() render_cubes(0)
 #define v(a) vertices[a]
-
 #define swap(a, b) do{int i = a; a = b; b = i;} while(0)
 
 void print_int(int, char, char);
+
+void render_cubes(unsigned short, unsigned short);
+void render_cube_wireframe(projected_cube, unsigned short);
+void render_cubes(unsigned short, unsigned short);
 
 void render_cube_wireframe(projected_cube projection, unsigned short color){
     LCD_DrawLine(projection.v(0).x, projection.v(0).y, 
@@ -45,16 +50,17 @@ void render_cube_wireframe(projected_cube projection, unsigned short color){
 
 void render_cube_fill(projected_cube projection, unsigned short color){
     int x, y;
-    //Draw front face
+    //Fill front face
     LCD_DrawFilledRect(projection.v(7).x, projection.v(7).y,
                        projection.v(2).x-projection.v(7).x,
                        projection.v(2).y-projection.v(7).y, color);
-    //Draw top face and side face
+    //Fill top face and side face
     LCD_DrawFilledRect(projection.v(4).x, projection.v(4).y,
                        projection.v(5).x-projection.v(4).x,
                        projection.v(7).y-projection.v(4).y, color);
     //If the cube is on the left or middle
     if (projection.v(7).x < projection.v(4).x) {
+        //Lefty top triangle
         for (x=projection.v(7).x; x < projection.v(4).x; ++x){
             for (y=projection.v(4).y; y < projection.v(7).y; ++y){
                 if ((projection.v(4).x-projection.v(7).x)*y <
@@ -68,10 +74,29 @@ void render_cube_fill(projected_cube projection, unsigned short color){
             LCD_DrawFilledRect(projection.v(6).x, projection.v(6).y,
                                projection.v(2).x-projection.v(6).x,
                                projection.v(2).y-projection.v(6).y, color);
+            //Upper right triangle
+            for (x=projection.v(6).x; x < projection.v(5).x; ++x){
+                for (y=projection.v(5).y; y < projection.v(6).y; ++y){
+                    if ((projection.v(5).x-projection.v(6).x)*y <
+                        (projection.v(6).y-projection.v(5).y)*x) {
+                        LCD_DrawPixel(x, y, color);
+                    }
+                }
+            }
+            //Lower right triangle
+            for (x=projection.v(2).x; x < projection.v(1).x; ++x){
+                for (y=projection.v(1).y; y < projection.v(2).y; ++y){
+                    if ((projection.v(1).x-projection.v(2).x)*y <
+                        (projection.v(2).y-projection.v(1).y)*x) {
+                        LCD_DrawPixel(x, y, color);
+                    }
+                }
+            }
         }
     }
     //If the cube is on the right or middle
     if (projection.v(5).x < projection.v(6).x) {
+        //Righty top triangle
         for (x=projection.v(5).x; x < projection.v(6).x; ++x){
             for (y=projection.v(5).y; y < projection.v(6).y; ++y){
                 if ((projection.v(6).x-projection.v(5).x)*y <
@@ -85,6 +110,24 @@ void render_cube_fill(projected_cube projection, unsigned short color){
             LCD_DrawFilledRect(projection.v(4).x, projection.v(7).y,
                                projection.v(7).x-projection.v(4).x,
                                projection.v(3).y-projection.v(7).y, color);
+            //Upper left triangle
+            for (x=projection.v(5).x; x < projection.v(7).x; ++x){
+                for (y=projection.v(5).y; y < projection.v(7).y; ++y){
+                    if ((projection.v(7).x-projection.v(5).x)*y <
+                        (projection.v(7).y-projection.v(5).y)*x) {
+                        LCD_DrawPixel(x, y, color);
+                    }
+                }
+            }
+            //Lower left triangle
+            for (x=projection.v(0).x; x < projection.v(3).x; ++x){
+                for (y=projection.v(0).y; y < projection.v(3).y; ++y){
+                    if ((projection.v(3).x-projection.v(0).x)*y <
+                        (projection.v(3).y-projection.v(0).y)*x) {
+                        LCD_DrawPixel(x, y, color);
+                    }
+                }
+            }
         }
     }
 }
